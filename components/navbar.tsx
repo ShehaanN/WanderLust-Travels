0397cc9602ee19fb3f77e2ff45d85dafc9088c9f
ShehaanN <shehannadeesha2580@gmail.com>
2025-11-27 16:19:16 +0530

    feat: add Booking page and update navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
           </ul>
 
           <Link
-            href="#"
+            href="/booking"
             className="hidden md:inline-block px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-600/90 transition-colors duration-200 font-medium"
           >
             Book Now
